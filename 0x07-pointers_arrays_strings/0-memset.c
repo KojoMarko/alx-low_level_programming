174919781a7566d7c62a075fff7b3282840cb4d4
KojoMarko <calebsenyah@gmail.com>
2022-03-30 03:22:21 +0000

    hmmmm such a welcome

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,11 @@ nclude "main.h"
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	while (n > 0)
+	for  (i = 0; i < n; i++)
 	{
-		s[i] = b;
-		i++;
-		n--;
+		*(s + i) = b;
 	}
 	return (s);
 }
